6d6bb52bc30c276a0c877f7ee1a67eb07915a44f
blackway76 <blackway76@gmail.com>
2015-10-04 20:27:33 +0900

    4장 연습 소스 완료

--- 4chapter/scrum/board/views.py
+++ 4chapter/scrum/board/views.py
@@ -1,10 +1,63 @@
-from rest_framework import viewsets
-from .models import Sprint
-from .serializers import SprintSerializer
+from django.contrib.auth import get_user_model
+from .forms import TaskFilter, SprintFilter
+from rest_framework import viewsets, authentication, permissions, filters
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
 
-class SprintViewSet(viewsets.ModelViewSet):
-    """API endpoint for listing and creating sprints."""
+User = get_user_model()
+
+
+class DefaultsMixin(object):
+    """Default settings for view authentication, permissions,
+    filltering and pagination."""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param = 'pagi_size'
+    max_paginate_by = 100
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating springs."""
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+    filter_class = SprintFilter
+    search_fileds = ('name',)
+    ordering_fileds = ('end','name',)
+
+
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_class = TaskFilter
+    #serializer_class = TaskSerializer
+    search_fileds = ('name','description',)
+    ordering_fileds = ('name','order','started','due','completed',)
+
+
+
+class UserViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
+    search_fileds = (User.USERNAME_FIELD,)
+
+
