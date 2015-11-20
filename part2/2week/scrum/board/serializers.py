be5737fcc23ab1c8d927230be5c50c704f2b156b
dwayne park <gregor77@naver.com>
2015-11-21 01:23:24 +0900

    REST API 작업중

--- part2/2week/scrum/board/serializers.py
+++ part2/2week/scrum/board/serializers.py
@@ -9,7 +9,7 @@ from .models import Sprint, Task
 User = get_user_model()
 
 class SprintSerializer(serializers.ModelSerializer):
-
+    
     links = serializers.SerializerMethodField()
 
     class Meta:
@@ -21,8 +21,10 @@ class SprintSerializer(serializers.ModelSerializer):
         return {
             'self': reverse('sprint-detail',
                 kwargs={'pk': obj.pk},request=request),
+            'tasks': reverse('task-list',
+                request=request) + '?sprint={}'.format(obj.pk),
         }
-
+    
 class TaskSerializer(serializers.ModelSerializer):
 
     assigned = serializers.SlugRelatedField(
@@ -38,11 +40,11 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_status_display(self, obj):
         return obj.get_status_display()
-
+    
     def get_links(self, obj):
         request = self.context['request']
         links = {
-            'self': reverse('task-detail',
+            'self': reverse('task-detail', 
                 kwargs={'pk': obj.pk}, request=request),
             'sprint': None,
             'assigned': None
@@ -56,7 +58,7 @@ class TaskSerializer(serializers.ModelSerializer):
         return links
 
 class UserSerializer(serializers.ModelSerializer):
-
+    
     full_name = serializers.CharField(source='get_full_name', read_only=True)
     links = serializers.SerializerMethodField()
 
@@ -71,4 +73,6 @@ class UserSerializer(serializers.ModelSerializer):
         return {
             'self': reverse('user-detail',
                 kwargs={User.USERNAME_FIELD: username}, request=request),
+            'tasks': '{}?assigned={}'.format(
+                reverse('task-list', request=request), username)
         }
